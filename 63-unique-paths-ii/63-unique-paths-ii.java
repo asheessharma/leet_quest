267019bdac654a340752b5bbc058a1ce1c140ef5
asheessharma <80744753+asheessharma@users.noreply.github.com>
2022-09-02 22:01:28 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (21.99%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -8,6 +8,7 @@ class Solution {
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             if(i==0 || j==0){
+                //its previous cell is obstacle or current cell is obstacle.
                 if(obstacleGrid[i][j]==1|| i!=0 && obstacleGrid[i-1][0]==0||j!=0 && obstacleGrid[i][j-1]==0){
                     obstacleGrid[i][j]=0;}
                 else{
