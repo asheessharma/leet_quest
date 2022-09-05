3984b2027767f992f5bf18f9f6a75f105e5e13c4
asheessharma <80744753+asheessharma@users.noreply.github.com>
2022-09-05 15:49:03 +0530

    Time: 0 ms (100.00%), Space: 39 MB (92.88%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,15 +1,16 @@
 class Solution {
     private int[]dp=new int[31];
     public int fib(int n) {
-        /*top down approach (memoization)
-        dp=recursion + caching     
-  ************************************************
+          
+       /* ******************************************
         if(n<=1)   //slowest approach --> pure recursion
             return n;
         if(n==2)
             return 1;
         return fib(n-1)+fib(n-2);
          ---------------------------------------*/
+         /*top down approach (memoization)
+        dp=recursion + caching  */
          if(n<=1)  
             return n;
         if(n==2)
