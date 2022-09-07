0fc8da9c27e32cabf21e9f286ddf4110fe4823a5
asheessharma <80744753+asheessharma@users.noreply.github.com>
2022-09-07 15:43:25 +0530

    Time: 5 ms (20.55%), Space: 45.9 MB (58.36%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -6,14 +6,14 @@ class Solution {
         int row=grid.length;
         int col=grid[0].length;
         int dp[][]=new int[row][col];
-        dp[0][0]=grid[0][0];
-        for(int i=1;i<row;i++){
+        dp[0][0]=grid[0][0];//first ele per cost same rahegi
+        for(int i=1;i<row;i++){//to fill first row[ele]=row[curr]+row[prev]
             dp[i][0]=dp[i-1][0]+grid[i][0];
         }
-        for(int j=1;j<col;j++){
+        for(int j=1;j<col;j++){//to fill first col[curr]+col[prev]
             dp[0][j]=dp[0][j-1]+grid[0][j];
         }
-        for(int i=1;i<row;i++){
+        for(int i=1;i<row;i++){//for remaining blocks grid[curr]+min of (upper ele,prev right)
             for(int j=1;j<col;j++){
                 dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
             }
