9e13e48ff82020c2eebcdd355033e2162a98e489
asheessharma <80744753+asheessharma@users.noreply.github.com>
2022-09-07 15:44:05 +0530

    Time: 2 ms (89.09%), Space: 46.1 MB (42.35%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,8 +1,7 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         if(grid==null || grid.length==0 || grid[0].length==0){
-            return 0;
-        }
+            return 0; }
         int row=grid.length;
         int col=grid[0].length;
         int dp[][]=new int[row][col];
