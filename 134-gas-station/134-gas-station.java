ef02cfe73b6285e9d081879cd7509249f2723e9c
asheessharma <80744753+asheessharma@users.noreply.github.com>
2022-09-14 15:58:48 +0530

    Time: 8 ms (14.18%), Space: 80 MB (69.26%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,19 +1,19 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-       int pos=-1;
+        int pos=-1;
         int sum=0;
         int curr=0;
         for(int i=0;i<gas.length;i++){
-            int diff=gas[i]-cost[i];
+            int diff=gas[i]-cost[i];//to complete a round --> total of diff in gas&cost (all station)>0
             curr+=diff;
             sum+=diff;
-            if(curr<0){
+            if(curr<0){//if current is negative then store position of that station
                 curr=0;
                 pos=i;
             }
         }
         if(sum>=0)
-            return pos+1;
+            return pos+1;//starting station= station whose diff(gas - cost)>0 
         return -1;
     }
 }
