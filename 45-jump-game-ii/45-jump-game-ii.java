8983ad431ddc1497018ea671c388aec4601264ea
asheessharma <80744753+asheessharma@users.noreply.github.com>
2022-09-15 05:17:14 +0530

    Time: 8 ms (46.61%), Space: 49.7 MB (36.35%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
         int n=nums.length; //top down approach 
-        int []dp=new int[n];
+      /*  int []dp=new int[n];
         Arrays.fill(dp,Integer.MAX_VALUE);
         dp[n-1]=0;
         for(int i=n-2;i>=0;i--){
@@ -12,6 +12,21 @@ class Solution {
                 dp[i]=min+1;
             }
         }
-        return dp[0];
+        return dp[0]; */
+        if(nums==null || n==0)
+            return 0;
+        int reach=0;
+        int lastReach=0;
+        int step=0;
+        for(int i=0;i<=reach && i<n;i++){
+            if(i>lastReach){
+                step++;
+                lastReach=reach;
+            }
+            reach=Math.max(reach,i+nums[i]);
+        }
+        if(reach<n-1)
+            return 0;
+        return step;
     }
 }
